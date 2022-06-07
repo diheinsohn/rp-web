2330b2b55a96313099cc972743c7bf399ca9c966
diheinsohn <diheinso@puc.cl>
2022-06-06 21:25:53 -0400

    Import fixes.

--- frontend/website/src/Layout.js
+++ frontend/website/src/Layout.js
@@ -2,11 +2,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 //Layout
-import Landing from 'pages/Landing';
-import Staking from 'pages/Staking';
+import Landing from './pages/Landing';
+import Staking from './pages/Staking';
 //Menu
-import TopMenu from 'components/TopMenu/TopMenu';
-import Footer from 'components/Footer/Footer';
+import TopMenu from './components/TopMenu/TopMenu';
+import Footer from './components/Footer/Footer';
 
 const Layout = (props) => {
   const { classes } = props;

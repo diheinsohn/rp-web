54248d4265152a9378e7b66d6108fc8a17d5ebed
Cory Cleaver <cory@beanworks.com>
2022-06-21 14:15:03 -0700

    Added styled components, restructured TopMenu, added FortAwesome for fontawesome icons, started work on cloud banner

--- website/src/App.js
+++ website/src/App.js
@@ -1,37 +1,35 @@
-import React from 'react';
+import React from "react";
 import Layout from "./Layout";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { withStyles } from "@mui/styles";
 
 const styles = {
   root: {
-    display: 'flex',
-    minHeight: '100vh',
-  }
-}
+    display: "flex",
+    minHeight: "100vh",
+  },
+};
 
 const mdTheme = createTheme({
   spacing: [0, 2, 3, 5, 8],
   palette: {
     primary: {
-      main: '#190C4D',
+      main: "#190C4D",
     },
     secondary: {
-      main: '#FF990F',
+      main: "#FF990F",
     },
   },
   typography: {
     allVariants: {
-      fontFamily: [
-        'Poppins',
-        'sans-serif',
-      ].join(','),
+      fontFamily: ["Poppins", "sans-serif"].join(","),
     },
+    fontSize: 16,
   },
   appContent: {
     flex: 1,
-    display: 'flex',
-    flexDirection: 'column',
+    display: "flex",
+    flexDirection: "column",
   },
   mainContent: {
     flex: 1,
@@ -39,23 +37,22 @@ const mdTheme = createTheme({
   overrides: {
     MuiButton: {
       raisedPrimary: {
-        color: 'white',
+        color: "white",
       },
     },
   },
-
 });
 
-const App = props => {
+const App = (props) => {
   const { classes } = props;
 
   return (
     <ThemeProvider theme={mdTheme}>
       <div className={classes.root}>
-        <Layout className={classes.appContent}/>
+        <Layout className={classes.appContent} />
       </div>
     </ThemeProvider>
   );
-}
+};
 
 export default withStyles(styles)(App);
